16a5aab02b9477260ed31541bdd4929cdb3256e7
Alex Berneau <haldir.alex@gmail.com>
2017-07-17 11:16:06 +0200

    refactor(boilerplate): refactor build

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,13 +1,4 @@
 // @flow
 
-const App = 'App';
-
-const test: string = 'test'
-
-console.log(test);
-
+const App: string = 'app'
 console.log(App);
-
-console.log('This is my awesome log');
-
-console.log('hello');
